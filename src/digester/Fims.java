41618bad125aac89005f5b02981d7b5560b43ac0
jdeck88 <jdeck88@a48a6a7d-09a7-8a3b-88bb-d95b5215db93>
2014-02-28 02:10:38 +0000

    updated configuration files to use the new http://flmnh-biscicol2.acis.ufl.edu/ds endpoint

--- src/digester/Fims.java
+++ src/digester/Fims.java
@@ -90,7 +90,7 @@ public class Fims implements RendererInterface {
             //fimsPrinter.out.println("\tBCID (directs to graph endpoint) =  " + bcid);
         } else {
             fimsPrinter.out.println("\tUnable to reach FIMS server for upload at " + metadata.getTarget() + ". " +
-                    "If this persists, your network may be blocking access to the database port 3030. " +
+                    "If this persists, your network may be blocking access to the database port. " +
                     "Please contact jdeck@berkeley.edu for more information.");
         }
     }
