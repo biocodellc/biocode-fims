345ba47e14127e7d25121e4083971aa4b17b10b2
jdeck88 <jdeck88@a48a6a7d-09a7-8a3b-88bb-d95b5215db93>
2014-08-01 19:18:10 +0000

    Removed the extractDataset functionality from biocode-fims.props file in the vanilla web directory.  This is only needed for SI Functionality... not needed for regular usage

--- web/js/biocode-fims.js
+++ web/js/biocode-fims.js
@@ -575,40 +575,6 @@ function extractProjectId() {
     }
 }
 
-// function to extract the dataset_code from a dataset to be uploaded
-function extractDatasetCode() {
-    var f = new FileReader();
-    // older browsers don't have a FileReader
-    if (f != null) {
-        var deferred = new $.Deferred();
-        var file = $('#dataset')[0].files[0];
-        // after file has been read, extract the project_id if present
-        f.onload = function () {
-            var fileContents = f.result;
-            var re = "~dataset_code=[a-zA-Z0-9-_]{4,16}~";
-            try {
-                var results = fileContents.match(re)[0];
-
-                if (results != null) {
-                    var dataset_code = results.split('=')[1].slice(0, -1);
-                    if (dataset_code != null && dataset_code.length > 0) {
-                        deferred.resolve(dataset_code);
-                    }
-                } else {
-                    deferred.resolve(null);
-                }
-            } catch (e) {
-                deferred.resolve(null);
-            }
-        };
-        f.readAsText(file);
-        return deferred.promise();
-    } else {
-        // can't find the dataset_code, so return null
-        return null;
-    }
-}
-
 // function to toggle the project_id and expedition_code inputs of the validation form
 function validationFormToggle() {
     $('#dataset').change(function() {
@@ -639,14 +605,7 @@ function validationFormToggle() {
         // only get expedition codes if a user is logged in
         if ($('*:contains("Logout")').length > 0) {
             $("#expedition_code").replaceWith("<p id='expedition_code'>Loading ... </p>");
-            $.when(extractDatasetCode()).done(function(dataset_code) {
-                if (dataset_code != null) {
-                    $("#expedition_code").replaceWith('<input type="hidden" name="expedition_code" id="expedition_code">'+dataset_code);
-                    $("#expedition_code").val(dataset_code);
-                } else {
-                    getExpeditionCodes();
-                }
-            });
+            getExpeditionCodes();
         }
     });
 }
