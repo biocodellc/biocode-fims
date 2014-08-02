0e0e59fe69c595a342916353ff2265553c0a6e4b
jdeck88 <jdeck88@a48a6a7d-09a7-8a3b-88bb-d95b5215db93>
2014-08-02 23:08:44 +0000

    New message for incoming validation data.

--- trunk/src/rest/validate.java
+++ trunk/src/rest/validate.java
@@ -361,10 +361,10 @@ public class validate {
 
                 // Copy file to a standard location
                 File inputFile = new File(processController.getInputFilename());
-                String outputFileName = "/opt/jetty_files/" +
-                        "project_" + processController.getProject_id() + "_" +
-                        "dataset_" + processController.getExpeditionCode() +
-                        ".xls";
+                String outputFileName = "/opt/jetty_files/" + inputFile.getName();
+                        //"project_" + processController.getProject_id() + "_" +
+                        //"dataset_" + processController.getExpeditionCode() +
+                        //".xls";
                 File outputFile = new File(outputFileName);
                 try {
                     copyFile(inputFile, outputFile);
@@ -397,10 +397,11 @@ public class validate {
                 processController.appendStatus("<br><font color=#188B00>Successfully Uploaded!</font>");
 
                 return "{\"done\": \"Successfully uploaded your spreadsheet to the server, <br>" +
-                        "server filename = " + outputFile.getName() + "<br>" +
+                        //"server filename = " + outputFile.getName() + "<br>" +
                         "dataset code = " + processController.getExpeditionCode() + "<br>" +
                         "dataset ARK = " + ark + "<br>" +
-                        "please maintain a local copy for now.  You should be notified of action soon.\"}";
+                        "Please maintain a local copy of your File!<br>" +
+                        "Your file will be processed soon for ingestion into RCIS.\"}";
 
             } catch (FIMSException e) {
                 e.printStackTrace();
