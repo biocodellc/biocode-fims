a5980939f7c23a0e9c9bfdb71115fe477c1e24a8
jdeck88 <jdeck88@a48a6a7d-09a7-8a3b-88bb-d95b5215db93>
2014-08-06 22:16:16 +0000

    adding a URL freshener to deal with Nameservers stubbornly holding cache

---
+++ trunk/src/utils/urlFreshener.java
@@ -0,0 +1,62 @@
+package utils;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+/**
+ * Always ensure a fresh URL with this nifty little class
+ */
+public class urlFreshener {
+    public static void main(String[]  args) {
+        urlFreshener t = new urlFreshener();
+
+        String url = "https://biocode-fims.googlecode.com/svn/trunk/TestResultsSITester.html";
+        try {
+            URL u = new URL(url);
+            System.out.println(t.forceLatestURL(u));
+
+        } catch (MalformedURLException e) {
+            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+        }
+    }
+
+    /**
+     * pass in a URL as a string
+     * @param url
+     * @return
+     */
+    public String forceLatestURL(String url) {
+        return url + queryDelimiter(url) + "v=" +System.currentTimeMillis() / 1000L;
+    }
+
+    /**
+     * pass in a URL as a URL
+     * @param url
+     * @return
+     * @throws MalformedURLException
+     */
+    public URL forceLatestURL(URL url) throws MalformedURLException {
+        return new URL(forceLatestURL(url.toString()));
+    }
+
+    /**
+     * Use the proper query delimiter ? or & depending on whether query conditions exist already or not
+     * @param urlString
+     * @return
+     */
+    private String queryDelimiter(String urlString) {
+        URL url = null;
+        try {
+            url = new URL(urlString);
+        } catch (MalformedURLException e) {
+            return "&";
+        }
+        if (url.getQuery() != null && !url.getQuery().equals("")) {
+            return "&";
+        } else {
+            return "?";
+        }
+    }
+
+
+}
