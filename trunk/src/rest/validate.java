3755403cea11ad510074d59bf336348b22577c34
jdeck88 <jdeck88@a48a6a7d-09a7-8a3b-88bb-d95b5215db93>
2014-08-12 03:21:48 +0000

    catch null better

--- trunk/src/rest/validate.java
+++ trunk/src/rest/validate.java
@@ -41,7 +41,7 @@ public class validate {
      */
     @POST
     @Consumes(MediaType.MULTIPART_FORM_DATA)
-    @Produces(MediaType.TEXT_HTML)
+    @Produces(MediaType.APPLICATION_JSON)
     public String validate(@FormDataParam("project_id") Integer project_id,
                            @FormDataParam("expedition_code") String expedition_code,
                            @FormDataParam("upload") String upload,
@@ -194,7 +194,7 @@ public class validate {
      */
     @GET
     @Path("/continue")
-    @Produces(MediaType.TEXT_HTML)
+    @Produces(MediaType.APPLICATION_JSON)
     public String upload(@QueryParam("createExpedition") @DefaultValue("false") Boolean createExpedition,
                          @Context HttpServletRequest request) {
         HttpSession session = request.getSession();
@@ -298,7 +298,7 @@ public class validate {
      */
     @GET
     @Path("/continue_spreadsheet")
-    @Produces(MediaType.TEXT_HTML)
+    @Produces(MediaType.APPLICATION_JSON)
     public String upload_spreadsheet(@QueryParam("createExpedition") @DefaultValue("false") Boolean createExpedition,
                                      @Context HttpServletRequest request) {
         HttpSession session = request.getSession();
@@ -428,7 +428,7 @@ public class validate {
      */
     @GET
     @Path("/status")
-    @Produces(MediaType.TEXT_HTML)
+    @Produces(MediaType.APPLICATION_JSON)
     public String status(@Context HttpServletRequest request) {
         HttpSession session = request.getSession();
 
