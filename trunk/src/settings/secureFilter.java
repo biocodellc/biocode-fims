4014d5c48aa59c028d9fa838185b92815337fc48
jdeck88 <jdeck88@a48a6a7d-09a7-8a3b-88bb-d95b5215db93>
2014-08-27 21:10:35 +0000

    security testing

---
+++ trunk/src/settings/secureFilter.java
@@ -0,0 +1,40 @@
+package settings;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+/**
+ * Filter which checks for the user in the session, if no user is present, the redirect to login page
+ */
+public class secureFilter implements Filter {
+    private FilterConfig fc = null;
+
+    public void init (FilterConfig fc)
+        throws ServletException {
+        this.fc = fc;
+    }
+
+    public void destroy() {
+        this.fc = null;
+    }
+
+    public void doFilter (ServletRequest req,
+                          ServletResponse res,
+                          FilterChain filterchain)
+        throws IOException, ServletException {
+
+        HttpServletRequest request = (HttpServletRequest) req;
+        HttpServletResponse response = (HttpServletResponse) res;
+
+        HttpSession session = request.getSession();
+
+        if (session.getAttribute("user") == null) {
+            response.sendRedirect("/fims/rest/authenticationService/login");
+        }
+        filterchain.doFilter(req, res);
+    }
+}
+
