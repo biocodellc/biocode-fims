d118b48ea0824c52709a36c489f3e4adde7c10c0
jdeck88 <jdeck88@a48a6a7d-09a7-8a3b-88bb-d95b5215db93>
2015-01-09 19:34:37 +0000

    Adding cspace query option

--- src/rest/query.java
+++ src/rest/query.java
@@ -154,6 +154,40 @@ public class query {
         }
     }
 
+     /**
+     * Return KML for a graph query using POST
+     * * <p/>
+     * filter parameters are of the form:
+     * name={URI} value={filter value}
+     *
+     * @return
+     */
+    @POST
+    @Path("/cspace/")
+    @Consumes("application/x-www-form-urlencoded")
+    @Produces("application/vnd.google-earth.kml+xml")
+    public Response queryCspace(
+            MultivaluedMap<String, String> form) {
+
+        // Build the query, etc..
+        fimsQueryBuilder q = POSTQueryResult(form);
+
+        // Run the query, passing in a format and returning the location of the output file
+        File file = new File(q.run("kml"));
+
+        // Return file to client
+        Response.ResponseBuilder response = Response.ok((Object) file);
+        response.header("Content-Disposition",
+                "attachment; filename=biocode-fims-output.xml");
+
+        // Return response
+        if (response == null) {
+            return Response.status(204).build();
+        } else {
+            return response.build();
+        }
+    }
+
     /**
      * Return Excel for a graph query.  The GET query runs a simple FILTER query for any term
      *
